691380447e2c9e961d96f41182c5509d5950dab3
YuZhiJian <641854024@QQ.COM>
2019-11-06 22:12:29 +0800

    还存bug

--- FileShow/Form1.Designer.cs
+++ FileShow/Form1.Designer.cs
@@ -35,6 +35,9 @@
             this.button2 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,9 +47,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(289, 21);
+            this.button1.Location = new System.Drawing.Point(385, 26);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 29);
             this.button1.TabIndex = 0;
             this.button1.Text = "选择文件夹";
             this.button1.UseVisualStyleBackColor = true;
@@ -54,23 +58,26 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(63, 21);
+            this.textBox1.Location = new System.Drawing.Point(84, 26);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(220, 21);
+            this.textBox1.Size = new System.Drawing.Size(292, 25);
             this.textBox1.TabIndex = 1;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(150, 60);
+            this.textBox3.Location = new System.Drawing.Point(200, 75);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(109, 21);
+            this.textBox3.Size = new System.Drawing.Size(144, 25);
             this.textBox3.TabIndex = 3;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(285, 60);
+            this.button2.Location = new System.Drawing.Point(380, 75);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(100, 29);
             this.button2.TabIndex = 4;
             this.button2.Text = "搜索文件";
             this.button2.UseVisualStyleBackColor = true;
@@ -79,32 +86,65 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(63, 110);
+            this.dataGridView1.Location = new System.Drawing.Point(84, 138);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(448, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(1215, 286);
             this.dataGridView1.TabIndex = 7;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(72, 65);
+            this.label1.Location = new System.Drawing.Point(96, 81);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.Size = new System.Drawing.Size(60, 15);
             this.label1.TabIndex = 8;
             this.label1.Text = "文件名:";
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(99, 112);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(104, 19);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "包括子文件";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(682, 76);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(51, 25);
+            this.textBox2.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(509, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 15);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "筛选匹配度：(默认0.8)";
+            // 
             // FileShow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(656, 369);
+            this.ClientSize = new System.Drawing.Size(1312, 638);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FileShow";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -122,6 +162,9 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- FileShow/Form1.cs
+++ FileShow/Form1.cs
@@ -37,10 +37,6 @@ namespace FileShow
             if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
             {
                 textBox1.Text = dialog.FileName.ToString();//获得完整路径在textBox1中显示
-                //StreamReader sr = new StreamReader(filename.FileName, Encoding.Default);//将/选/中的文件在textBox2中显示
-                //textBox2.Text = sr.ReadToEnd();
-                //sr.Close();
-                //string[] filenames = Directory.GetFiles(dialog.FileName.ToString(), "*", SearchOption.AllDirectories);
 
             }
         }
@@ -49,18 +45,51 @@ namespace FileShow
         {
             var fileName = textBox3.Text;
             string files = textBox1.Text;
-            string[] filenames = Directory.GetFiles(files, "*", SearchOption.AllDirectories);
+            //string[] filenames = Directory.GetFiles(files, "*", SearchOption.AllDirectories);
+            var isChecked = checkBox1.Checked;
+            var option = SearchOption.TopDirectoryOnly;
+            if (isChecked)
+            {
+                option = SearchOption.AllDirectories;
+            }
+            string[] filenames = Directory.GetFiles(files, "*", option);
+            var filterCount = 0.8;
+            if (!string.IsNullOrEmpty(textBox2.Text))
+            {
+                var isDouble = double.TryParse(textBox2.Text, out filterCount);
+                if (!isDouble || filterCount > 1)
+                {
+                    filterCount = 0.8;
+                }
+            }
             if (!string.IsNullOrWhiteSpace(fileName))
             {
                 filenames = filenames.Where(p => p.Contains(fileName)).ToArray();
             }
-            //string filenames_Enter = string.Join("\r\n", filenames);
-            //textBox2.Text = filenames_Enter;
+            FilterSimilar filter = new FilterSimilar();
+            List<string> lists = new List<string>();
+            for (int i = 0; i < filenames.Length; i++)
+            {
+                if (i == filenames.Length - 1)
+                    break;
+                var file1 = Path.GetFileNameWithoutExtension(filenames[i]);
+                var file2 = Path.GetFileNameWithoutExtension((filenames[i + 1]));
+                var fileName1 = Path.GetFileName(filenames[i]);
+                var fileName2 = Path.GetFileName((filenames[i + 1]));
+                var similarVal = filter.getSimilarityRatio(file1, file2);
+                if (similarVal >= filterCount)
+                {
+                    if (!lists.Contains(fileName1))
+                        lists.Add(fileName1);
+                    if (!lists.Contains(fileName2))
+                        lists.Add(fileName2);
+                }
+            }
             dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            dataGridView1.DataSource = (from s in filenames
+            dataGridView1.DataSource = (from s in lists
                                         select new
                                         {
-                                            fileName = Path.GetFileName(s)
+                                            fileName = s//Path.GetFileName(s)
                                         })
                                         .ToList();
         }
