9edfb97018acd35b96c9a8d57daf1bc499c743f9
yzj <641854024@example.com>
2019-11-06 17:48:12 +0800

    添加项目文件。

---
+++ FileShow/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace FileShow
+{
+    public partial class FileShow : Form
+    {
+        public FileShow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialog filename = new OpenFileDialog();
+            //filename.InitialDirectory = Application.StartupPath;
+            //filename.Filter = "Folders|\n";
+            //filename.AddExtension = false;
+            //filename.CheckFileExists = false;
+            //filename.DereferenceLinks = true;
+            //filename.Multiselect = false;
+            //filename.RestoreDirectory = true;
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = "C:\\Users";
+            dialog.IsFolderPicker = true;
+            dialog.Multiselect = false;
+            dialog.RestoreDirectory = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                textBox1.Text = dialog.FileName.ToString();//获得完整路径在textBox1中显示
+                //StreamReader sr = new StreamReader(filename.FileName, Encoding.Default);//将/选/中的文件在textBox2中显示
+                //textBox2.Text = sr.ReadToEnd();
+                //sr.Close();
+                //string[] filenames = Directory.GetFiles(dialog.FileName.ToString(), "*", SearchOption.AllDirectories);
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var fileName = textBox3.Text;
+            string files = textBox1.Text;
+            string[] filenames = Directory.GetFiles(files, "*", SearchOption.AllDirectories);
+            if (!string.IsNullOrWhiteSpace(fileName))
+            {
+                filenames = filenames.Where(p => p.Contains(fileName)).ToArray();
+            }
+            //string filenames_Enter = string.Join("\r\n", filenames);
+            //textBox2.Text = filenames_Enter;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.DataSource = (from s in filenames
+                                        select new
+                                        {
+                                            fileName = Path.GetFileName(s)
+                                        })
+                                        .ToList();
+        }
+    }
+}
---
+++ FileShow/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileShow
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FileShow());
+        }
+    }
+    public class similar
+    {
+
+        private int compare(String str, String target)
+        {
+            //int[][] d;
+            int[,] d;
+            int n = str.Length;
+            int m = target.Length;
+            int i; // 遍历str的
+            int j; // 遍历target的
+            char ch1; // str的
+            char ch2; // target的
+            int temp; // 记录相同字符,在某个矩阵位置值的增量,不是0就是1
+
+            if (n == 0) { return m; }
+            if (m == 0) { return n; }
+            d = new int[n + 1, m + 1];
+            for (i = 0; i <= n; i++)
+            { // 初始化第一列
+                d[i, 0] = i;
+            }
+            for (j = 0; j <= m; j++)
+            { // 初始化第一行
+                d[0, j] = j;
+            }
+            for (i = 1; i <= n; i++)
+            { // 遍历str
+                ch1 = str[i - 1];
+                // 去匹配target
+                for (j = 1; j <= m; j++)
+                {
+                    ch2 = target[j - 1];
+                    if (ch1 == ch2)
+                    {
+                        temp = 0;
+                    }
+                    else
+                    {
+                        temp = 1;
+                    }
+                    // 左边+1,上边+1, 左上角+temp取最小
+                    d[i, j] = min(d[i - 1, j] + 1, d[i, j - 1] + 1, d[i - 1, j - 1] + temp);
+                }
+            }
+            return d[n, m];
+        }
+        private int min(int one, int two, int three)
+        {
+            return (one = one < two ? one : two) < three ? one : three;
+        }
+        public float getSimilarityRatio(String str, String target)
+        {
+            return 1 - (float)compare(str, target) / Math.Max(str.Length, target.Length);
+        }
+    }
+}
